7372125b8d5a7da4ae7b422ab4e9dcc4ba00c48c
Paul1005 <mccarliep@gmail.com>
2018-11-09 22:31:06 -0800

    added objects

---
+++ BallisticsSandbox/Objects/Bullet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallisticsSandbox.Objects
+{
+    class Bullet
+    {
+        public Bullet(double diameter, double weight) {
+            this.Weight = weight;
+            this.Diameter = diameter;
+        }
+
+        public double Diameter { get; set; }
+        public double Weight { get; set; }
+    }
+}
---
+++ BallisticsSandbox/Objects/Surface.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BallisticsSandbox.Objects
+{
+    class Surface
+    {
+        public Surface(double thickness, double density, double angle)
+        {
+            this.Thickness = thickness;
+            this.Density = density;
+            this.Angle = angle;
+        }
+
+        public double Angle { get; set; }
+        public double Density { get; set; }
+        public double Thickness { get; set; }
+    }
+}
